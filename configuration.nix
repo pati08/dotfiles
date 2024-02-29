12b11957008f78f050ed856ceba206fa57376f91
Patrick Oberholzer <patrickoberholzer08@gmail.com>
2024-02-28 20:18:25 -0500

    19 current  2024-02-28 20:14:42  23.11.4801.b7ee09cf5614  6.1.79                          *

--- configuration.nix
+++ configuration.nix
@@ -129,10 +129,10 @@
     sync.enable = true;
 
     # Make sure to use the correct Bus ID values for your system!
-    nvidiaBusId = "PCI:14:0:0";
-    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+    intelBusId = "PCI:0:1:0";
   };
-  hardware.nvidia.forceFullCompositionPipeline = true;
+  # hardware.nvidia.forceFullCompositionPipeline = true;
 
   # Enable the fish shell
   # programs.fish.enable = true;
