e2ed50af8b116177b1eb9f93ca7915a133fbf4fd
Patrick Oberholzer <patrickoberholzer08@gmail.com>
2024-03-07 20:04:21 -0500

    45 current  2024-03-07 20:04:17  23.11.4801.b7ee09cf5614  6.1.79                          *

--- configuration.nix
+++ configuration.nix
@@ -124,6 +124,7 @@
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     xclip
+    libsForQt5.qt5.qtgraphicaleffects
     git
     gh
     gparted
