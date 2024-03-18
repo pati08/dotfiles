27299e3b50901bbc4f94471487a8bd3514e162b9
Patrick Oberholzer <patrickoberholzer08@gmail.com>
2024-03-18 15:28:50 -0400

    64 current  2024-03-18 15:28:45  23.11.5408.8ac30a39abc5  6.1.82                          *

--- configuration.nix
+++ configuration.nix
@@ -6,190 +6,196 @@
   pkgs,
   ...
 }: let
-  system-specific-packages = import ./system-specific.nix pkgs;
+  system-specific-config = import ./system-specific-conf.nix {
+    config = config;
+    pkgs = pkgs;
+  };
+  system-specific-packages = import ./system-specific-pkgs.nix pkgs;
   unstable = import <nixos-unstable> {config = {allowUnfree = true;};};
-in {
-  imports = [
-    # Include the results of the hardware scan.
-    /etc/nixos/hardware-configuration.nix
-  ];
+in
+  system-specific-config
+  // {
+    imports = [
+      # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+    ];
 
-  swapDevices = [
-    {
-      device = "/var/lib/swapfile";
-      size = 16 * 1024;
-    }
-  ];
+    swapDevices = [
+      {
+        device = "/var/lib/swapfile";
+        size = 16 * 1024;
+      }
+    ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    # Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "patrick-desktop"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    networking.hostName = "patrick-desktop"; # Define your hostname.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+    # Enable networking
+    networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "America/New_York";
+    # Set your time zone.
+    time.timeZone = "America/New_York";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+    # Enable the X11 windowing system.
+    services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+    # Enable the KDE Plasma Desktop Environment.
+    services.xserver.displayManager.sddm.enable = true;
+    services.xserver.desktopManager.plasma5.enable = true;
 
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
+    # Configure keymap in X11
+    services.xserver = {
+      layout = "us";
+      xkbVariant = "";
+    };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    # Enable sound with pipewire.
+    sound.enable = true;
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+    # Enable touchpad support (enabled default in most desktopManager).
+    # services.xserver.libinput.enable = true;
 
-  users.users.patrick = {
-    isNormalUser = true;
-    description = "Patrick Oberholzer";
-    extraGroups = ["networkmanager" "wheel"];
-    packages = with pkgs;
+    users.users.patrick = {
+      isNormalUser = true;
+      description = "Patrick Oberholzer";
+      extraGroups = ["networkmanager" "wheel"];
+      packages = with pkgs;
+        [
+          firefox
+          spotify
+          wezterm
+          ripgrep
+          zellij
+          bat
+          lsd
+          loc
+          celluloid
+          fzf
+          usbutils
+          file
+          obsidian
+        ]
+        ++ system-specific-packages.user;
+    };
+
+    nixpkgs.config.permittedInsecurePackages = ["electron-25.9.0"];
+
+    fonts.packages = with pkgs; [
+      (unstable.nerdfonts.override {fonts = ["FiraCode" "JetBrainsMono"];})
+    ];
+
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
+    };
+
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs;
       [
-        firefox
-        spotify
-        wezterm
-        ripgrep
-        zellij
-        bat
-        lsd
-        loc
-        celluloid
-        fzf
-        usbutils
-        file
-        obsidian
+        #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+        wget
+        xclip
+        git
+        gh
+        postgresql
+        gcc
+        openjdk
+        alejandra
+        fish
+        direnv
+        dunst
+        starship
+        pkg-config
+        openssl.dev
+        inotify-tools
+        libnotify
       ]
-      ++ system-specific-packages.user;
-  };
+      ++ system-specific-packages.system;
 
-  nixpkgs.config.permittedInsecurePackages = ["electron-25.9.0"];
+    nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  fonts.packages = with pkgs; [
-    (unstable.nerdfonts.override {fonts = ["FiraCode" "JetBrainsMono"];})
-  ];
+    # Enable the fish shell
+    programs.fish.enable = true;
+    programs.bash = {
+      interactiveShellInit = ''
+        if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+          then
+            shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+            exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+          fi
+      '';
+    };
 
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-  };
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # programs.mtr.enable = true;
+    # programs.gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+    # List services that you want to enable:
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs;
-    [
-      #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-      wget
-      xclip
-      git
-      gh
-      postgresql
-      gcc
-      openjdk
-      alejandra
-      fish
-      direnv
-      dunst
-      starship
-      pkg-config
-      openssl.dev
-      inotify-tools
-      libnotify
-    ]
-    ++ system-specific-packages.system;
+    services.lorri.enable = true;
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+    # Enable the OpenSSH daemon.
+    services.openssh.enable = true;
 
-  # Enable the fish shell
-  programs.fish.enable = true;
-  programs.bash = {
-    interactiveShellInit = ''
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-        then
-          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-          exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-        fi
-    '';
-  };
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  services.lorri.enable = true;
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
-}
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "23.11"; # Did you read the comment?
+  }
