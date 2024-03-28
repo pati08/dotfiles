4f8b82b0193b2aa4b14cece22c5b4da136909b0b
Patrick Oberholzer <patrickoberholzer08@gmail.com>
2024-03-28 07:00:19 -0400

    change nvidia drivers

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -100,7 +100,8 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    # package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = pkgs.linuxKernel.packages.linux_6_6.nvidia_x11;
 
     prime = {
       sync.enable = true;
