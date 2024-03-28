bb27966efb30388803d5202d35ca6d0ba7a06902
Patrick Oberholzer <patrickoberholzer08@gmail.com>
2024-03-27 20:27:34 -0400

    -

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -70,7 +70,7 @@
   };
 
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia-dkms"]; # or "nvidiaLegacy470 etc.
+  services.xserver.videoDrivers = ["nvidia"]; # or "nvidiaLegacy470 etc.
 
   hardware.nvidia = {
     # Modesetting is required.
