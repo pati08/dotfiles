90edf196840922d20d5798f467e760a611655a49
Patrick Oberholzer <patrickoberholzer08@gmail.com>
2024-03-30 10:17:19 -0400

    starting to move to HM

--- configuration.nix
+++ configuration.nix
@@ -180,19 +180,27 @@ in {
     ]
     ++ system-specific-packages.system;
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  # nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix = {
+    settings = {
+      package = pkgs.nixFlakes;
+      extraOptions = ''
+        experimental-features = nix-command flakes
+      '';
+    };
+  };
 
   # Enable the fish shell
-  programs.fish.enable = true;
-  programs.bash = {
-    interactiveShellInit = ''
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-        then
-          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-          exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-        fi
-    '';
-  };
+  # programs.fish.enable = true;
+  # programs.bash = {
+  #   interactiveShellInit = ''
+  #     if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+  #       then
+  #         shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+  #         exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+  #       fi
+  #   '';
+  # };
 
   # Hyprland
   programs.hyprland = {
