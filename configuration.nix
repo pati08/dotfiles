326e03f80a40e786d5dd0a6a7df99bba1a0c8fc3
Patrick Oberholzer <patrickoberholzer08@gmail.com>
2024-05-19 16:45:26 -0400

    idrk

--- configuration.nix
+++ configuration.nix
@@ -21,8 +21,11 @@ in {
 
   nix.settings = {
     # enable hyperland's cachix
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    substituters = ["https://nix-community.cachix.org" "https://hyprland.cachix.org"];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
 
     # enable flakes
     experimental-features = ["nix-command" "flakes"];
