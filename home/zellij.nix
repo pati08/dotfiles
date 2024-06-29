6de6a9bc6a33d0f6c3cb975f530218db031484cb
Patrick Oberholzer <patrickoberholzer08@gmail.com>
2024-06-29 14:52:40 -0400

    zellij changes

--- home/zellij.nix
+++ home/zellij.nix
@@ -6,4 +6,6 @@ in
   programs.zellij.enable = true;
 
   home.file.".config/zellij/config.kdl".source = ./zellij.kdl;
+
+  home.file.".config/zellij/layouts/default.kdl".source = ./zellij-layout.kdl;
 }
